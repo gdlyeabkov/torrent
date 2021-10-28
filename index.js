4f04cf906464e25dda2945dc65b0938bc1e5f770
gdlyeabkov <gdlyeabkov@gmail.com>
2021-10-28 20:45:54 +0300

    addsearchpage

---
+++ index.js
@@ -0,0 +1,104 @@
+const fs = require('fs')
+const multer  = require('multer')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+})
+const upload = multer({ storage: storage })
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/groups')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+})
+const upload = multer({ storage: storage })
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const mongoose = require('mongoose')
+const express = require('express')
+const path = require('path')
+const { isError } = require('util')
+const serveStatic = require('serve-static')
+
+const app = express()
+
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+const url = `mongodb+srv://glebClusterUser:glebClusterUserPassword@cluster0.fvfru.mongodb.net/distributions?retryWrites=true&w=majority`;
+
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+
+mongoose.connect(url, connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
+
+const DistributionSchema = new mongoose.Schema({
+    name: String,
+    forum: String,
+    author: String,
+    size: Number,
+    seeds: String,
+    leechs: String,
+    added: {
+        type: Date,
+        default: Date.now
+    },
+    checked: {
+        type: Boolean,
+        default: false
+    },
+    downloaded: {
+        type: Number,
+        default: 0
+    },
+}, { collection : 'mydistributions' });
+
+const DistributionModel = mongoose.model('DistributionModel', DistributionSchema);
+
+app.get('/api/distributions/get',(req, res)=>{
+    
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, X-Access-Token, X-Socket-ID, Content-Type");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+   
+    let query = DistributionModel.findById(req.query.distributionid)
+    query.exec((err, Distribution) => {
+        if (err){
+            return
+        }
+        return res.json({ status: "OK" })
+    })
+
+})
+
+app.get('**', (req, res) => { 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, X-Access-Token, X-Socket-ID, Content-Type");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    
+    return res.redirect(`http://localhost:4000/?redirectroute=${req.path}`)
+})
+
+// const port = process.env.PORT || 8080
+const port = 4000  
+
+app.listen(port)
---
+++ src/views/Search.vue
@@ -0,0 +1,137 @@
+<template>
+    <div>
+    <Header />
+    <div class="main">
+        <div class="resultsInfo">
+            <span class="resultsInfoHeader">
+                Трекер
+            </span>
+            <span class="resultsInfoCountOfResults">
+                Страницы:  1
+            </span>
+            <span>
+                <span class="resultsInfoCountOfResults">
+                    Результатов поиска: 13
+                </span>
+                 (max: 500) 
+            </span>
+        </div>
+        <div class="searchByDistributions">
+            <div class="searchByDistributionsHeader">
+                <span>
+                    Поиск по раздачам
+                </span>
+            </div>
+            <div class="searchByDistributionsFilter">
+                <div class="goToSection">
+                    <span>
+                        Перейти к разделу
+                    </span>
+                </div>
+                <div class="sortBy">
+                    <div class="sortByRowHeader">
+                        <div>
+                            <div class="sortByThem">
+
+                            </div>
+                            <div class="torrentFor">
+                                <span>
+                                    Торренты за
+                                </span>
+                                <select class="form-select h-75 w-25" aria-label="Default select example">
+                                    <option selected>за всё время</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="showOnly">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="btn btn-light searchBtn">
+                Поиск
+            </button>
+        </div>
+    </div>
+    <Footer />
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+    name: 'Search',
+    components: {
+        Header,
+        Footer
+    }
+}
+</script>
+
+<style scoped>
+    .resultsInfo {
+        display: flex;
+        flex-direction: column;
+        margin: 15px 0px;
+    }
+
+    .resultsInfoHeader {
+        font-size: 24px;
+    }
+        
+    .resultsInfoCountOfResults {
+        font-weight: bolder;
+    }
+
+    .main {
+        margin: auto;
+        width: 98%;
+    }
+
+    .searchByDistributionsHeader {
+        height: 35px;
+        background-color: rgb(0, 100, 255);
+        font-weight: bolder;
+        color: rgb(100, 200, 255);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .searchByDistributions {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .searchBtn {
+        font-weight: bolder;
+        border: 1px solid rgb(150, 150, 150);
+        width: 125px;
+        align-self: center;
+    }
+
+    .searchByDistributionsFilter {
+        margin: 25px 0px;
+        width: 75%;
+        display: flex;
+        justify-content: center;
+        margin: auto;
+    }
+
+    .goToSection, .sortBy {
+        box-sizing: border-box;
+        padding: 10px;
+        margin: 15px;
+        width: 50%;
+        height: 350px;
+        border: 1px solid rgb(150, 150, 150);
+    }
+
+    .goToSection {
+        overflow-y: scroll;
+    }
+
+</style>
